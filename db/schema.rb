91749e4e2e7b1914a0bb1a4738210e4ea00f31b1
kkunvar-singh <kunvarsingh507@gmail.com>
2023-06-07 20:02:36 +0530

    database changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_02_212546) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "addresses", force: :cascade do |t|
     t.string "parmanent_address"
     t.string "residencial_address"
@@ -21,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_212546) do
     t.string "state"
     t.string "country"
     t.string "pin"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
