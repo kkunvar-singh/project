2bb2d30004a7c843ace91958db76ef81c418aa2f
kkunvar-singh <kunvarsingh507@gmail.com>
2023-06-07 15:00:36 +0530

    update database yml to pg

--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_212546) do
-  create_table "addresses", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2023_06_03_125617) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
+
+  create_table "addresses", id: :uuid, default: nil, force: :cascade do |t|
     t.string "parmanent_address"
     t.string "residencial_address"
     t.string "city"
     t.string "state"
     t.string "country"
-    t.string "pin"
-    t.integer "user_id", null: false
+    t.integer "pin"
+    t.uuid "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table "educations", force: :cascade do |t|
-    t.string "institude_name"
-    t.string "university"
-    t.string "course"
-    t.string "stream"
-    t.string "marks"
-    t.string "passed_year"
-    t.integer "parsentage"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: nil, force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.integer "mobail_number"
     t.string "email"
     t.string "date_of_birth"
-    t.string "activated", default: "true"
+    t.integer "activated"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
     t.string "otp"
     t.datetime "otp_send_at"
     t.integer "type_otp"
